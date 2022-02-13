cd03020d1bd0e01b64547d93f5e9e6d44f277fb2
Alik Ovesyan <icoleksander.Ovesian@americanwell.com>
2022-02-13 15:50:37 +0200

    fixes

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,10 @@ const selectMoviesTypeContainer: any = document.querySelector(
 	'.select-movie-rates'
 );
 const filterGenreBody: any = document.querySelector('.filter-genre-body');
+const searchBtn = document.querySelector('.search-btn');
+const selectMoviesType = document.querySelector('.select-movie-rates');
+const applyFilterBtn = document.querySelector('.apply-genres-btn');
+const detailBackBtn = document.querySelector('.detail-back-button');
 
 const API_URL = 'https://api.themoviedb.org/3';
 const API_KEY = 'e1036725d4b220e51c48c798d13bcf37';
@@ -22,11 +26,11 @@ const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/w500';
 
 let currentPage = 1;
 let genres: any = [];
-const pages: any = [];
 let paginationNumberArr: any = [];
 let pageIndex = 0;
 let currentRequestType = 'top_rated';
 
+const pages: any = [];
 const moviesType = [
 	{ value: 'top_rated', title: 'Top Rated' },
 	{ value: 'popular', title: 'Popular' },
@@ -113,25 +117,23 @@ const getGenres = async () => {
 	} catch (err) {}
 };
 
-const pageHanlder = (action: any, page: any) => {
+const pageHanlder = (action: any, event: any) => {
+	const page = parseInt(event.target.getAttribute('data-page-i'));
+
 	if (action === 'pageClick') {
 		currentPage = page;
-	}
-	if (action === 'nextPage') {
-		currentPage = currentPage + 1;
-	}
-	if (action === 'previousPage') {
-		currentPage = currentPage - 1;
 	}
 
 	getMoviesOnCondition(currentRequestType, currentPage, action);
 };
 
-const onMovieClick = (id: any) => {
+const onMovieClick = (event: any) => {
 	movieContentList.style.display = 'none';
 	movieDetails.style.display = 'flex';
 
-	getMovieById(id);
+	const movieId = event.target.parentElement.getAttribute('data-movie-id');
+
+	getMovieById(movieId);
 };
 
 const onBack = () => {
@@ -149,7 +151,9 @@ const onSelectMoviesType = async (event: any) => {
 	getMoviesOnCondition(event.target.value, currentPage, 'selectMoviesType');
 };
 
-const onChooseGenre = (genreId: any) => {
+const onChooseGenre = (event: any) => {
+	const genreId = event.target.getAttribute('data-genre-id');
+
 	if (genresToSend.length === 0) {
 		genresToSend.push(genreId);
 	} else {
@@ -175,7 +179,8 @@ const renderMovie = (movies: any) => {
 	movieWrapper.innerHTML = '';
 
 	movies.forEach((movie: any) => {
-		movieWrapper.innerHTML += `<div onclick="onMovieClick(${movie.movieId})" class="movie-card">
+		// NEW !!!
+		movieWrapper.innerHTML += `<div data-movie-id="${movie.movieId}" class="movie-card">
 			<img src="${movie.image}" class="movie-logo-img" />
 			<div class="movie-details-info">
 					<p class="movie-title">${movie.title}</p>
@@ -184,48 +189,57 @@ const renderMovie = (movies: any) => {
 			</div>
 		</div>`;
 	});
+
+	// NEW !!!
+	document.querySelectorAll('.movie-card').forEach(mc => {
+		mc.addEventListener('click', onMovieClick, false);
+	});
 };
 
 const renderDetailsPage = (movieById: any) => {
 	detailsContainer.innerHTML = '';
 
 	detailsContainer.innerHTML += `	<div class="movie-vote-average-container">
-	<img
-		class="movie-img"
-		src="${movieById.image}"
-	/>
-	<div class="vote-average-container">
-	<p class="vote-average-header">Vote Average:</p>
-		<p class="vote-average-sign">${movieById.voteAverage}</p>
-	</div>
-
-</div>
-<div class="movie-details-sign-container">
-	<div class="title-details">
-		<p class="movie-details-sign">Title:</p>
-		<p class="detail-info-sign detail-title">${movieById.title}</p>
-	</div>
-	<div>
-		<p class="movie-details-sign">Year:</p>
-		<p class="detail-info-sign">${movieById.year}</p>
-	</div>
-	<div>
-		<p class="movie-details-sign">Genre:</p>
-		<p class="detail-info-sign">${movieById.genre}</p>
-	</div>
-	<div>
-		<p class="movie-details-sign">Overview:</p>
-		<p class="detail-overview">${movieById.overview}</p>
-	</div>
-</div>`;
+    <img
+      class="movie-img"
+      src="${movieById.image}"
+    />
+    <div class="vote-average-container">
+    <p class="vote-average-header">Vote Average:</p>
+      <p class="vote-average-sign">${movieById.voteAverage}</p>
+    </div>
+  </div>
+  <div class="movie-details-sign-container">
+    <div class="title-details">
+      <p class="movie-details-sign">Title:</p>
+      <p class="detail-info-sign detail-title">${movieById.title}</p>
+    </div>
+    <div>
+      <p class="movie-details-sign">Year:</p>
+      <p class="detail-info-sign">${movieById.year}</p>
+    </div>
+    <div>
+      <p class="movie-details-sign">Genre:</p>
+      <p class="detail-info-sign">${movieById.genre}</p>
+    </div>
+    <div>
+      <p class="movie-details-sign">Overview:</p>
+      <p class="detail-overview">${movieById.overview}</p>
+    </div>
+  </div>`;
 };
 
 const renderGenres = () => {
 	genres.forEach((genre: any) => {
 		filterGenreBody.innerHTML += `<div class="body-filter-body">	
-		<input type="checkbox" onclick="onChooseGenre(${genre.id})" />
+		<input type="checkbox" data-genre-id="${genre.id}" class="filtered-checkboxes" />
 		<p class="body-filter-movie-type">${genre.name}</p>
 	</div>`;
+	});
+
+	// NEW !!!
+	document.querySelectorAll('.filtered-checkboxes').forEach(fc => {
+		fc.addEventListener('click', onChooseGenre, false);
 	});
 };
 
@@ -253,16 +267,9 @@ const renderPages = (totalPages: any, action: any) => {
 	}
 
 	if (
-		!paginationNumberArr.includes(currentPage) &&
-		action === 'nextPage' &&
-		currentPage !== totalPages - 4
-	) {
-		pageIndex = currentPage + 5;
-	}
-
-	if (
-		!paginationNumberArr.includes(currentPage) &&
-		(action === 'previousPage' || action === 'pageClick')
+		paginationNumberArr[0] === currentPage &&
+		currentPage !== 1 &&
+		action === 'pageClick'
 	) {
 		if (currentPage === 4) {
 			pageIndex = 0;
@@ -274,15 +281,32 @@ const renderPages = (totalPages: any, action: any) => {
 	paginationNumberArr = pages.slice(pageIndex, pageIndex + 5);
 
 	for (let i = 0; i < paginationNumberArr.length; i++) {
-		const markup = `<button onclick="pageHanlder('pageClick', ${
+		const markup = `<button data-page-i="${
 			paginationNumberArr[i]
-		})" class="pagination ${
+		}" class="pagination ${
 			currentPage === paginationNumberArr[i] ? 'active' : ''
 		}">${paginationNumberArr[i]}</button>`;
 		paginationContainer.innerHTML += markup;
 	}
+
+	// NEW !!!
+	document.querySelectorAll('.pagination').forEach(p => {
+		p.addEventListener(
+			'click',
+			(event: any) => {
+				pageHanlder('pageClick', event);
+			},
+			false
+		);
+	});
 };
 
 renderMoviesTypes();
 getGenres();
 getMoviesOnCondition('top_rated', currentPage, 'getMovies');
+
+// NEW !!!
+searchBtn.addEventListener('click', onSearch, false);
+selectMoviesType.addEventListener('change', onSelectMoviesType, false);
+applyFilterBtn.addEventListener('click', onApplyFilter, false);
+detailBackBtn.addEventListener('click', onBack, false);
